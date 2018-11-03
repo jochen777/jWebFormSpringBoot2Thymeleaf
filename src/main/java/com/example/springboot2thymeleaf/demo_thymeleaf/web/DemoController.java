995220c772e7e767211a716cb9839cdd7d28b0ad
Jochen Pier <jpier@me.com>
2018-11-03 18:20:28 +0100

    Align with refactorings

--- src/main/java/com/example/springboot2thymeleaf/demo_thymeleaf/web/DemoController.java
+++ src/main/java/com/example/springboot2thymeleaf/demo_thymeleaf/web/DemoController.java
@@ -1,46 +1,45 @@
 package com.example.springboot2thymeleaf.demo_thymeleaf.web;
 
-import jwebform.field.SubmitType;
-import jwebform.integration.annotations.UseDecoration;
-import jwebform.integration.annotations.UseFieldType;
-import jwebform.spring.SimpleJWebForm;
-import lombok.extern.slf4j.Slf4j;
+import javax.validation.constraints.NotEmpty;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.constraints.NotEmpty;
+import jwebform.field.SubmitType;
+import jwebform.integration.ContainerFormRunner;
+import jwebform.integration.bean2form.annotations.UseDecoration;
+import jwebform.integration.bean2form.annotations.UseFieldType;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class DemoController {
 
   /*
-   an autoconfigured Argument resolver will fill the DemoForm with the request params
-   and put the formModel in the model as "form" and "form_rendered"
-  */
+   * an autoconfigured Argument resolver will fill the DemoForm with the request params and put the
+   * formModel in the model as "form" and "form_rendered"
+   */
   @RequestMapping("/demo")
-  public void demo(SimpleJWebForm<DemoForm> form, Model model) {
-    if (form.isOk()){
+  public void demo(ContainerFormRunner<DemoForm> form, Model model) {
+    if (form.isSubmittedAndOk()) {
       model.addAttribute("success", "YES");
       log.info("Form was successfully submitted: " + form.getBean().firstname);
     }
   }
 
   /*
-   Demo form.
-   Note: This must be "public static" here, because it have to be initialsed programmatically
-   See for handling beans: https://github.com/jochen777/jWebForm/blob/master/doc/beans.md
-    */
+   * Demo form. Note: This must be "public static" here, because it have to be initialsed
+   * programmatically See for handling beans:
+   * https://github.com/jochen777/jWebForm/blob/master/doc/beans.md
+   */
   public static class DemoForm {
 
     @UseDecoration(label = "Your firstname", helpText = "Don't cheat here!")
-    public String firstname="";
+    public String firstname = "";
 
     @NotEmpty
-    public String lastname="";
+    public String lastname = "";
 
     @UseFieldType(SubmitType.class)
-    public String submit="";
+    public String submit = "";
   }
 }
