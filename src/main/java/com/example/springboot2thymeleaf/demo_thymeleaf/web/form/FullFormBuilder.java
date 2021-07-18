85709a66460a38dd787ca187e7906c9cad8b5465
Jochen Pier <jpier@me.com>
2021-07-18 10:04:09 +0200

    Start new eleemnts

--- src/main/java/com/example/springboot2thymeleaf/demo_thymeleaf/web/form/FullFormBuilder.java
+++ src/main/java/com/example/springboot2thymeleaf/demo_thymeleaf/web/form/FullFormBuilder.java
@@ -3,19 +3,69 @@ package com.example.springboot2thymeleaf.demo_thymeleaf.web.form;
 import jwebform.Form;
 import jwebform.FormBuilder;
 import jwebform.processor.FormGenerator;
+import jwebform.validation.Criterion;
 import jwebform.validation.criteria.Criteria;
+
+import java.time.LocalDate;
 
 import static jwebform.field.builder.BuildInType.*;
 
 public class FullFormBuilder implements FormGenerator {
+
+    Criterion req = Criteria.required();
+
     @Override
     public Form generateForm() {
             return FormBuilder.simple().typeBuilder(
-                    text("name").
-                            label("name").
-                            criteria(Criteria.required()),
-                    checkbox("optin").
-                            label("checkbox.label"),
+                    text           ("textInput", "Peter\"Paul").
+                            criteria     (req).
+                            label        ("TextInputLabel"),
+
+                    textDate       ("dateInput", LocalDate.of(2017, 7, 4)).
+                            criteria     (req).
+                            label        ("date_input").
+                            helpText     ("datehelptext"),
+
+                    text           ("textInput2", "Peter\"Paul").
+                            criteria     (req).
+                            label        ("TextInputLabel2").
+                            helpText     ("Help-Text").
+                            placeholder  ("Placeholder"),
+
+                    select         ("gender", "", new String[] {"m", "f"}, new String[] {"Male", "Female"}).
+                            label        ("Gender"),
+
+
+                    checkbox       ("chk", true).
+                            criteria     (req).
+                            label        ("chk-label").
+                            helpText     ("chk_help"),
+
+                    label          ("lbl"),
+
+                    html           ("<strong>HTML</strong>"),
+
+                    hidden         ("hddn", "hddn-value"),
+
+                    textArea       ("area", "Area-Prebuild").
+                            criteria     (req).
+                            label        ("Area").
+                            helpText     ("Area-Help").
+                            placeholder  ("Area-Placeholder"),
+
+                    number         ("nbr", 5).
+                            criteria     (req).
+                            label        ("nbr-label").
+                            helpText     ("nrb-help"),
+
+                    password       ("pssword").
+                            label        ("Password"),
+
+                    upload         ("upld").
+                            label        ("Upload"),
+
+                    radio          ("radio", "1", new String[] {"1", "2"}, new String[] {"yes", "no"})
+                            .label       ("Radio"),
                     submit("ok").
                             label("ok")
             ).build();
